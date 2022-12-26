a346cb5ec06c3fabc9058a718164f5f5ac22fc09
Ameena Hassan <ameenahassan1024@gmail.com>
2022-12-26 17:26:20 +0000

    Day01b - found what the top three elves carry

---
+++ Day1/Day01b.java
@@ -0,0 +1,63 @@
+package Day1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day01b {
+
+  private static final String FILENAME = "/Users/main/Git_Projects/2022_advent-of-code/Day1/day01a/input.txt";
+
+  public static void main(String[] args) throws IOException {
+
+    File file = new File(FILENAME);
+
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+      List<Elf> allElvesList = new ArrayList<Elf>();
+
+      Elf currentElf = new Elf();
+
+      while (true) {
+        String tmp = bufferedReader.readLine();
+
+        if (tmp == null) {
+          // EOF!
+          // add the current Elf to the list of elves, and break.
+          if (currentElf.calories != 0) {
+            allElvesList.add(currentElf);
+          }
+          break;
+        } else if (tmp.isEmpty()) {
+          // newline!
+
+          /**
+           * The current elf is already up to date;
+           * we just have to add it to the list if
+           * it has any calories!
+           **/
+          if (currentElf.calories != 0) {
+            allElvesList.add(currentElf);
+          }
+          currentElf = new Elf();
+
+        } else {
+          // String! (of ints)
+          int calories = Integer.parseInt(tmp);
+          currentElf.addMoreCalories(calories);
+        }
+      }
+
+      if (!allElvesList.isEmpty()) {
+        Collections.sort(allElvesList);
+        List<Elf> topThreeElfs = allElvesList.subList(0, 3);
+        int topThreeCarry = topThreeElfs.stream().mapToInt(e -> e.calories).sum();
+        System.out.println("The top three Elves carry " + topThreeCarry + " calories with them.\n");
+      }
+    }
+  }
+
+}
