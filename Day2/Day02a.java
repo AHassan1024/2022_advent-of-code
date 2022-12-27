ece84ab135fd65bea9c27562915ca38ed19dca5e
Ameena Hassan <ameenahassan1024@gmail.com>
2022-12-27 01:02:00 +0000

    Added input and filepath to Day2a - main

---
+++ Day2/Day02a.java
@@ -0,0 +1,15 @@
+package Day2;
+
+import java.io.File;
+
+public class Day02a {
+
+  private static final String FILENAME = "/Users/main/Git_Projects/2022_advent-of-code/Day2/day02a/input.txt";
+
+  public static void main(String[] args) {
+
+    File file = new File(FILENAME);
+
+  }
+
+}
