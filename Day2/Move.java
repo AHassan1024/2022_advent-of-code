43360c8d2ba8f49d622c8553c7a153059fed63b0
Ameena Hassan <ameenahassan1024@gmail.com>
2022-12-27 00:57:45 +0000

    Moves have added Strings, score

--- Day2/Move.java
+++ Day2/Move.java
@@ -1,9 +1,31 @@
 package Day2;
 
 public enum Move {
-  ROCK,
-  PAPER,
-  SCISSORS;
+  ROCK("A", "X", 1),
+  PAPER("B", "Y", 2),
+  SCISSORS("C", "Z", 3);
+
+  private final String oppMoveString;
+  private final String youMoveString;
+  private final int score;
+
+  Move(String oppMoveString, String youMoveString, int score) {
+    this.oppMoveString = oppMoveString;
+    this.youMoveString = youMoveString;
+    this.score = score;
+  }
+
+  private String oppMoveString() {
+    return oppMoveString;
+  }
+
+  private String youMoveString() {
+    return youMoveString;
+  }
+
+  private int score() {
+    return score;
+  }
 
   public Move ifOppThenYouMove(Move opp) {
     switch (opp) {
