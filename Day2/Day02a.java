77a202ae19086ed3d39479e90473f7339e4b16bd
Ameena Hassan <ameenahassan1024@gmail.com>
2022-12-27 01:31:09 +0000

    Adding simple buffered file reader

--- Day2/Day02a.java
+++ Day2/Day02a.java
@@ -1,6 +1,9 @@
 package Day2;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Day02a {
 
@@ -9,6 +12,26 @@ public class Day02a {
   public static void main(String[] args) {
 
     File file = new File(FILENAME);
+
+    // Use a buffered reader to read each round invididually.
+
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+
+      while (true) {
+        String round = bufferedReader.readLine();
+        // new object round with two moves?
+        if (round == null) {
+          // EOF!
+          break;
+        }
+
+        // we now start the rounds ...
+
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
   }
 
