f31322a4b2c7367f2437030bc97c138ad147f4fb
Ameena Hassan <ameenahassan1024@gmail.com>
2022-12-28 01:00:31 +0000

    Adding Day3a skeleton

--- Day3/Day03a.java
+++ Day3/Day03a.java
@@ -2,4 +2,14 @@ package Day3;
 
 public class Day03a {
 
+  public static void main(String[] args) {
+    // Read each line in the input.txt file
+    // Break down each line into a Rucksack, with two String containers
+    // Create a List of Rucksacks
+    // Compare the two container strings to create a priority character
+    // convert each priority character into int
+    // sum the priorities
+
+  }
+
 }
