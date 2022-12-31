efea3e526c03faf2d06047e02c963556f7a9c364
Ameena Hassan <ameenahassan1024@gmail.com>
2022-12-31 15:59:07 +0000

    Can just use .chars().distinct()

--- Day6/Day06a.java
+++ Day6/Day06a.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class Day06a {
 
-  private static final String FILENAME = "/Users/main/Git_Projects/2022_advent-of-code/Day6/src/test_input.txt";
+  private static final String FILENAME = "/Users/main/Git_Projects/2022_advent-of-code/Day6/src/input.txt";
 
   private static final int PACKET = 4;
 
@@ -21,11 +21,13 @@ public class Day06a {
     try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
       String inputString = bufferedReader.readLine();
 
-      int i = 0;
-      while ((i < inputString.length() - PACKET + 1) && (!allDifferent(inputString.substring(i, i + 4)))) {
-        i++;
+      for (int i = 0, j = PACKET; j < inputString.length(); i++, j++) {
+        if (allDifferent(inputString.substring(i, j))) {
+          System.out.println(j);
+          break;
+        }
       }
-      System.out.println(i);
+      // System.out.println(i);
 
       // Create objects, if needed
 
@@ -40,26 +42,6 @@ public class Day06a {
   }
 
   private static boolean allDifferent(String four) {
-    int i = 0;
-    int j = four.length() - 1;
-
-    boolean diff = false;
-    while (true) {
-      if (i == four.length() - 1) {
-        break;
-      }
-
-      if (i != j && (four.charAt(i) == four.charAt(j))) {
-        diff = true;
-        break;
-      } else {
-        j--;
-        if (j == 0) {
-          j = four.length() - 1;
-          i++;
-        }
-      }
-    }
-    return diff;
+    return (four.chars().distinct().count() == four.length());
   }
 }
